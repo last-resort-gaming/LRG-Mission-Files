b4d8d5a50473f5a7e9036d30cad09c23cc901fa4
Mitch PC <cartwright1993@live.com>
2018-06-18 12:52:30 +0100

    - Included YAINA FPS monitor

--- Template Files/Functions/YAINA/General/CfgFunctions.hpp
+++ Template Files/Functions/YAINA/General/CfgFunctions.hpp
@@ -3,6 +3,8 @@ class YAINA {
 
 	class General {
 		file = "Functions\YAINA\General";
+		class crewNames { postInit = 1; };
 		class formatDuration {};
+		class showFPS { postInit = 1; };
     };
 };
