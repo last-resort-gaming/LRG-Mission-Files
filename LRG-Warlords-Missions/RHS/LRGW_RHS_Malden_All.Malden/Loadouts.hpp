3a79f881f9e5a08278dc9e138d678f1e3ebacc40
Mitch <cartwright1993@live.com>
2019-03-03 00:46:26 +0000

    Included Loadouts.hpp

---
+++ LRG-Warlords-Missions/RHS/LRGW_RHS_Malden_All.Malden/Loadouts.hpp
@@ -0,0 +1,190 @@
+// RHS US
+class rhsusf_army_ocp_rifleman
+{
+    displayName = "Rifleman";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "";
+    weapons[] = {"rhs_weap_m4_carryhandle","rhsusf_weap_m1911a1","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhsusf_mag_7x45acp_MHP","rhsusf_mag_7x45acp_MHP","rhs_mag_m67","rhs_mag_m67","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhsusf_iotv_ocp_Medic","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_T1_high","","","","",""};
+};
+class rhsusf_army_ocp_maaws
+{
+    displayName = "Anti Tank";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "";
+    weapons[] = {"rhs_weap_m4_carryhandle","rhs_weap_M136","rhsusf_weap_glock17g4","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_m67","rhs_mag_m67","rhsusf_mag_17Rnd_9x19_JHP","rhsusf_mag_17Rnd_9x19_JHP","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhsusf_iotv_ocp_Rifleman","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_T1_high","","","","",""};
+};
+class rhsusf_army_ocp_aa
+{
+    displayName = "Anti Air";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "rhsusf_assault_eagleaiii_ocp";
+    weapons[] = {"rhs_weap_m4_carryhandle","rhs_weap_fim92","rhsusf_weap_glock17g4","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_m67","rhs_mag_m67","rhsusf_mag_17Rnd_9x19_JHP","rhsusf_mag_17Rnd_9x19_JHP","SmokeShell","SmokeShell","rhs_fim92_mag"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhsusf_iotv_ocp_Rifleman","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_T1_high","","","","",""};
+};
+class rhsusf_army_ocp_autorifleman
+{
+    displayName = "Auto Rifleman";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "rhsusf_assault_eagleaiii_ocp_ar";
+    weapons[] = {"rhs_weap_m249_pip_S","rhsusf_weap_m9","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_m67","rhs_mag_m67","rhsusf_mag_15Rnd_9x19_JHP","rhsusf_mag_15Rnd_9x19_JHP","SmokeShell","SmokeShell","rhs_200rnd_556x45_T_SAW","rhsusf_100Rnd_556x45_soft_pouch","rhsusf_100Rnd_556x45_soft_pouch"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhsusf_iotv_ocp_SAW","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_T1_high","","","","",""};
+};
+
+class rhsusf_army_ocp_marksman
+{
+    displayName = "Marksman";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "";
+    weapons[] = {"rhs_weap_m14ebrri","rhsusf_weap_glock17g4","Binocular","Throw","Put"};
+    magazines[] = {"rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhs_mag_m67","rhs_mag_m67","rhsusf_mag_17Rnd_9x19_JHP","rhsusf_mag_17Rnd_9x19_JHP","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhsusf_iotv_ocp_Rifleman","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_ACOG","","","","",""};
+};
+
+class rhsusf_army_ocp_medic
+{
+    displayName = "Medic";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "rhsusf_assault_eagleaiii_ocp_medic";
+    weapons[] = {"rhs_weap_m4_carryhandle","rhsusf_weap_m1911a1","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhsusf_mag_7x45acp_MHP","rhsusf_mag_7x45acp_MHP","rhs_mag_m67","rhs_mag_m67","SmokeShell","SmokeShell","SmokeShell","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit","Medikit"};
+    linkedItems[] = {"rhsusf_iotv_ocp_Medic","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_T1_high","","","","",""};
+};
+class rhsusf_army_ocp_Engineer
+{
+    displayName = "Engineer";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_cu_ocp";
+    backpack = "rhsusf_assault_eagleaiii_ocp_engineer";
+    weapons[] = {"rhs_weap_m4_carryhandle","rhsusf_weap_m1911a1","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhsusf_mag_7x45acp_MHP","rhsusf_mag_7x45acp_MHP","rhs_mag_m67","rhs_mag_m67","SmokeShell","SmokeShell","SmokeShell","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit","Toolkit","DemoCharge_Remote_Mag","DemoCharge_Remote_Mag"};
+    linkedItems[] = {"rhsusf_iotv_ocp_Medic","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhsusf_ANPVS_14","","rhsusf_acc_anpeq15A","rhsusf_acc_T1_high","","","","",""};
+};
+// RHS Russia
+class rhs_msv_emr_rifleman
+{
+    displayName = "Rifleman";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "";
+    weapons[] = {"rhs_weap_ak74m","rhs_weap_makarov_pm","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_rgn","rhs_mag_rgn","rhs_mag_9x18_8_57N181S","rhs_mag_9x18_8_57N181S","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_mag_rgn","rhs_mag_rgn","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhs_6b23_digi_medic","rhs_6b27m_digi","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","rhs_acc_dtk","rhs_acc_perst1ik","rhs_acc_pkas","","","","",""};
+};
+
+class rhs_msv_emr_at
+{
+    displayName = "Anti Tank";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "";
+    weapons[] = {"rhs_weap_ak74m","rhs_weap_rpg26","rhs_weap_pya","Binocular","Throw","Put"};
+    magazines[] = {"rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_mag_rgn","rhs_mag_rgn","rhs_mag_9x19_17","rhs_mag_9x19_17"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhs_6b23_digi_rifleman","rhs_6b27m_green","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","rhs_acc_dtk","rhs_acc_2dpZenit","rhs_acc_pkas","","","","",""};
+};
+
+class rhs_msv_emr_aa
+{
+    displayName = "Anti Air";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "";
+    weapons[] = {"rhs_weap_ak74m","rhs_weap_igla","rhs_weap_pya","Binocular","Throw","Put"};
+    magazines[] = {"rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_mag_rgn","rhs_mag_rgn","rhs_mag_9x19_17","rhs_mag_9x19_17"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhs_6b23_digi_rifleman","rhs_6b27m_green","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","rhs_acc_dtk","rhs_acc_2dpZenit","rhs_acc_pkas","","","","",""};
+};
+
+class rhs_msv_emr_arifleman
+{
+    displayName = "Auto Rifleman";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "rhs_sidorMG";
+    weapons[] = {"rhs_weap_pkp","rhs_weap_pya","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_rgn","rhs_mag_rgo","rhs_mag_rgo","SmokeShell","SmokeShell","rhs_mag_9x19_17","rhs_mag_9x19_17","rhs_100Rnd_762x54mmR","rhs_100Rnd_762x54mmR"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhs_6b23_digi_6sh92","rhs_6b27m_digi","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","","","rhs_acc_pkas_pkp","","","","",""};
+};
+
+class rhs_msv_emr_marksman
+{
+    displayName = "Marksman";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "";
+    weapons[] = {"rhs_weap_svds","rhs_weap_pp2000_folded","Binocular","Throw","Put"};
+    magazines[] = {"SmokeShell","SmokeShell","rhs_10Rnd_762x54mmR_7N1","rhs_10Rnd_762x54mmR_7N1","rhs_10Rnd_762x54mmR_7N1","rhs_10Rnd_762x54mmR_7N1","rhs_10Rnd_762x54mmR_7N1","rhs_mag_9x19mm_7n21_20","rhs_mag_9x19mm_7n21_20","rhs_mag_rgn","rhs_mag_rgn"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit"};
+    linkedItems[] = {"rhs_6b23_digi_sniper","rhs_6b26","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","","","rhs_acc_pso1m2","","","","",""};
+};
+
+class rhs_msv_emr_medic
+{
+    displayName = "Medic";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "rhs_medic_bag";
+    weapons[] = {"rhs_weap_ak74m","rhs_weap_makarov_pm","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_rgn","rhs_mag_rgn","rhs_mag_9x18_8_57N181S","rhs_mag_9x18_8_57N181S","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","SmokeShell","SmokeShell","SmokeShell","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit","Medikit"};
+    linkedItems[] = {"rhs_6b23_digi_medic","rhs_6b27m_digi","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","rhs_acc_dtk","rhs_acc_perst1ik","rhs_acc_pkas","","","","",""};
+};
+
+class rhs_msv_emr_engineer
+{
+    displayName = "Engineer";
+    icon = "z\LRG Fundamentals\addons\Media\images\Patches\LRGLogo.paa";
+    role = "default";
+    show = "true";
+    uniformClass = "rhs_uniform_msv_emr";
+    backpack = "rhs_assault_umbts_engineer_empty";
+    weapons[] = {"rhs_weap_ak74m","rhs_weap_makarov_pm","Binocular","Throw","Put"};
+    magazines[] = {"rhs_mag_rgn","rhs_mag_rgn","rhs_mag_9x18_8_57N181S","rhs_mag_9x18_8_57N181S","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_AK","SmokeShell","SmokeShell","SmokeShell","SmokeShell","SmokeShell"};
+    items[] = {"FirstAidKit","FirstAidKit","FirstAidKit","FirstAidKit","Toolkit"};
+    linkedItems[] = {"rhs_6b23_digi_medic","rhs_6b27m_digi","","ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS","rhs_1PN138","rhs_acc_dtk","rhs_acc_perst1ik","rhs_acc_pkas","","","","",""};
+};
