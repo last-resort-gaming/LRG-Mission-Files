4052b0471f3e0bcef44751ce16b62254a4ac99eb
Last Resort Gaming <lastresortgamingeu@gmail.com>
2020-04-24 18:07:19 +0100

    Update Helfa_Arth_1.ps1 mission name

--- LRG-Operations/Official/036 - Helfa Arth/Helfa_Arth_I.vt7.ps1
+++ LRG-Operations/Official/036 - Helfa Arth/Helfa_Arth_I.vt7.ps1
@@ -8,8 +8,8 @@ $Headless_Clients = 1	# (0,1)			DEFAULT: 1	Number of Headless Clients to boot on
 $Enable3rdPerson  = 0	# (0,1)			DEFAULT: 0	Usually disabled on Ops, enabled on public server.											
 
 #Mission Parameters
-$Password = 'DownInTheWoods'								#Password used by clients to connect to the server.
-$MissionFolder = 'Operation_Helfa_Arth_Mission_1.vt7'		#Exact name of mission folder e.g. MyMission.Altis
+$Password = 'DownInTheWoods'			#Password used by clients to connect to the server.
+$MissionFolder = 'Helfa_Arth_I.vt7'		#Exact name of mission folder e.g. MyMission.Altis
 
 $ClientMods = 'C:\Mods\@LAMBS_Danger.fsm;C:\Mods\@LRG Fundamentals;C:\Mods\@NATO_Rus_Vehicle;C:\Mods\@Project Infinite - All in one;C:\Mods\@Project Infinite compat ACE3;C:\Mods\@Radio Animations for Task Force Radio;C:\Mods\@RHSUSAF;C:\Mods\@RUSSOF_LITE_mas;C:\Mods\@ShackTac User Interface;C:\Mods\@Suppress;C:\Mods\@task_force_radio;C:\Mods\@Virolahti - Valtatie 7;C:\Mods\@ace;C:\Mods\@ACE Compat - RHS United States Armed Forces;C:\Mods\@ACE Interaction Menu Expansion;C:\Mods\@ACEX;C:\Mods\@CBA_A3;C:\Mods\@CTab Blufor Tracker v2.2.3;C:\Mods\@CUP Terrains - Core;C:\Mods\@Enhanced Movement;C:\Mods\@FA-18 Super Hornet;C:\Mods\@Finnish Defense Forces uniforms;C:\Mods\@FIR AWS(AirWeaponSystem);C:\Mods\@Hellenic Armed Forces Mod (HAFM) - Helicopters;C:\Mods\@ILBE Assault Pack - Rewrite;C:\Mods\@KA Suitcase Nuke'
 $ServerMods = 'C:\Mods\@Advanced Rappelling;C:\Mods\@Advanced Sling Loading;C:\Mods\@Advanced Urban Rappelling;C:\Mods\@infiSTAR_A3_vision EU3;C:\Mods\@A3Log'
