30ea6e48206219fe6e74ef1da6442dd0d397a89c
Last Resort Gaming <lastresortgamingeu@gmail.com>
2020-04-24 16:27:47 +0100

    Fixes for the YAINA launch files

--- LRG-YAINA/YAINA_Modded.Altis.ps1
+++ LRG-YAINA/YAINA_Modded.Altis.ps1
@@ -1,22 +1,23 @@
 #Server Parameters
-$Server = 1				# Which Server to use. Can be 1, 2, 3, 4, 5														DEFAULT: 3
-$Clear_Logs = 1			# If set to 1 will clear all previous server logs. Should be enabled unless fault finding. 		DEFAULT: 1
-$Copy_Keys = 1			# If set to 1 all available keys will be copied to directory. 									DEFAULT: 0
+$Server = 1				# (1,2,3,4,5) 	DEFAULT: 3	Which Server to use. 														
+$Clear_Logs = 1			# (0,1) 		DEFAULT: 1	If set to 1 will clear all previous server logs. Should be enabled unless fault finding. 		
+$verifySignatures = 1	# (0,1)			DEFAULT: 0	Check the client has the same mods loaded as the server. Enable for public servers.			
+$EnableVON = 1			# (0,1)			DEFAULT: 0	Enable in game VON channels (Side chat, command chat ect). Enable for non TFAR servers.		
+$EnableBattleye = 1		# (0,1)			DEFAULT: 0	Enabled Battleye anti-cheat. Should always be on for public servers, optional on Ops.			
+$Headless_Clients = 0	# (0,1)			DEFAULT: 1	Number of Headless Clients to boot on the server. 											
+$Enable3rdPerson  = 1	# (0,1)			DEFAULT: 0	Usually disabled on Ops, enabled on public server.								DEFAULT: 0
 
 #Mission Parameters
 $Password = ''								#Password used by clients to connect to the server.
 $MissionFolder = 'YAINA_Modded.Altis'		#Exact name of mission folder e.g. MyMission.Altis
-$Headless_Clients = 0										#Number if Headless Clients to boot on the server. 			DEFAULT: 1
 
 $ClientMods = 'C:\Mods\@3CB BAF Equipment;C:\Mods\@3CB BAF Units;C:\Mods\@3CB BAF Units (ACE Compatibility);C:\Mods\@3CB BAF Vehicles;C:\Mods\@3CB BAF Weapons;C:\Mods\@ace;C:\Mods\@CBA_A3;C:\Mods\@LRG Fundamentals;C:\Mods\@RKSL Studios- Attachments v3.01;C:\Mods\@task_force_radio'
 $ServerMods = 'C:\Mods\@a3graphite;C:\Mods\@A3Log;C:\Mods\@Achilles;C:\Mods\@Advanced Rappelling;C:\Mods\@Advanced Sling Loading;C:\Mods\@Advanced Towing;C:\Mods\@Advanced Urban Rappelling;C:\Mods\@asct;C:\Mods\@CBA_A3;C:\Mods\@infiSTAR_A3;C:\Mods\@infiSTAR_A3_vision EU1;C:\Mods\@inidbi2;C:\Mods\@Server side smoke (for Blastcore Edited);C:\Mods\@yaina_Modded'
 
-set-location -path 'C:\Servers\Arma 3\YAINA'
 git fetch
-git pull
-Start-Process -NoNewWindow -Wait -Filepath "bin\yaina.bat" -ArgumentList "generate -a -v Modded"
+git pull --recurse-submodules
+git submodule update --remote
+Start-Process -NoNewWindow -Wait -Filepath "src\bin\yaina.bat" -ArgumentList "generate -a -v Modded"
 
-
-D:\LoginDetails.ps1 $Server $Clear_Logs $Copy_Keys $Password $MissionFolder $Headless_Clients $ClientMods $ServerMods
-
+D:\LoginDetails.ps1 $Server $Clear_Logs $verifySignatures $EnableVON $EnableBattleye $Headless_Clients $Enable3rdPerson $Password $MissionFolder $ClientMods $ServerMods
 
