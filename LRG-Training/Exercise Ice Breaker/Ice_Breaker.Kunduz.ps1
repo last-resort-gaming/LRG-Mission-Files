c52a2d963bb1620df92905e9789664063066d7c2
Last Resort Gaming <lastresortgamingeu@gmail.com>
2020-06-23 15:59:40 +0100

    Update Ice Breaker PS File

--- LRG-Training/Exercise Ice Breaker/Ice_Breaker.Kunduz.ps1
+++ LRG-Training/Exercise Ice Breaker/Ice_Breaker.Kunduz.ps1
@@ -1,18 +1,16 @@
 #Server Parameters
-$Server = 2				# (1,2,3) 		DEFAULT: 2	1 Public, 2 Private, 3 Testing/Spare.													
-
-$verifySignatures = 0	# (0,1)			DEFAULT: 0	Check the client has the same mods loaded as the server. Enable for public servers.			
-$EnableVON = 0			# (0,1)			DEFAULT: 0	Enable in game VON channels (Side chat, command chat ect). Enable for non TFAR servers.		
-$EnableBattleye = 0		# (0,1)			DEFAULT: 0	Enabled Battleye anti-cheat. Should always be on for public servers, optional on Ops.			
-$Headless_Clients = 0	# (0,1)			DEFAULT: 1	Number of Headless Clients to boot on the server. 											
-$Enable3rdPerson  = 0	# (0,1)			DEFAULT: 0	Usually disabled on Ops, enabled on public server.											
-
+$Server = 2
+$verifySignatures = 0
+$EnableVON = 0
+$EnableBattleye = 0
+$Headless_Clients = 1
+$Enable3rdPerson = 0
 #Mission Parameters
-$Password = 'LRGTraining'						#Password used by clients to connect to the server.
-$MissionFolder = 'Ice_Breaker.Kunduz'			#Exact name of mission folder e.g. MyMission.Altis
+$Password = 'LRGTraining'
+$MissionFolder = 'Ice_Breaker.Kunduz'
 
-$ClientMods = 'C:\Mods\@3CB BAF Vehicles;C:\Mods\@ShackTac User Interface;C:\Mods\@task_force_radio;C:\Mods\@3CB BAF Equipment;C:\Mods\@3CB BAF Units;C:\Mods\@3CB BAF Units (ACE compatibility);C:\Mods\@3CB BAF Weapons;C:\Mods\@ace;C:\Mods\@ACE Compat - RHS United States Armed Forces;C:\Mods\@ACE Interaction Menu Expansion;C:\Mods\@ACEX;C:\Mods\@CBA_A3;C:\Mods\@Enhanced Movement;C:\Mods\@Kunduz Afghanistan - Fixed Doors;C:\Mods\@LRG Fundamentals;C:\Mods\@RHSUSAF;C:\Mods\@RKSL Studios- Attachments v3.01'
-$ServerMods = 'C:\Mods\@Advanced Rappelling;C:\Mods\@Advanced Sling Loading;C:\Mods\@Advanced Urban Rappelling'
-$OptionalMods = 'C:\Mods\@Achilles;C:\Mods\@BackpackOnChest;C:\Mods\@Blastcore Edited (standalone version);C:\Mods\@CBA_A3;C:\Mods\@DynaSound 2;C:\Mods\@Enhanced Movement;C:\Mods\@Enhanced Soundscape;C:\Mods\@JSRS SOUNDMOD;C:\Mods\@ShackTac User Interface;C:\Mods\@ZEC - Zeus and Eden Templates - Building Compositions;C:\Mods\@ZEI - Zeus and Eden Interiors;C:\Mods\@Zeus Enhanced'
+$ClientMods = 'C:\ModsGenerated\450814997;C:\ModsGenerated\1661066023;C:\ModsGenerated\893328083;C:\ModsGenerated\893339590;C:\ModsGenerated\893349825;C:\ModsGenerated\893346105;C:\ModsGenerated\463939057;C:\ModsGenerated\843577117;C:\ModsGenerated\1135539579;C:\ModsGenerated\498740884;C:\ModsGenerated\620019431;C:\ModsGenerated\773125288;C:\ModsGenerated\708250744;C:\ModsGenerated\1448755472;C:\ModsGenerated\1376867375;C:\ModsGenerated\333310405;C:\ModsGenerated\1515845502;C:\ModsGenerated\1188303655;C:\ModsGenerated\767380317;C:\ModsGenerated\861133494;C:\ModsGenerated\825174634'
+$ServerMods = 'C:\ModsGenerated\713709341;C:\ModsGenerated\615007497;C:\ModsGenerated\639837898;C:\ModsGenerated\730310357'
+$OptionalMods = 'C:\ModsGenerated\450814997;C:\ModsGenerated\1779063631;C:\ModsGenerated\1251859358;C:\ModsGenerated\642912021;C:\ModsGenerated\498740884;C:\ModsGenerated\1480333388;C:\ModsGenerated\825179978;C:\ModsGenerated\333310405;C:\ModsGenerated\825181638;C:\ModsGenerated\820924072;C:\ModsGenerated\723217262;C:\ModsGenerated\767380317;C:\ModsGenerated\861133494'
 
 D:\LoginDetails.ps1 $Server $verifySignatures $EnableVON $EnableBattleye $Headless_Clients $Enable3rdPerson $Password $MissionFolder $ClientMods $ServerMods $OptionalMods
